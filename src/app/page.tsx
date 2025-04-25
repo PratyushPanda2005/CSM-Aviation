256fc600f7f3c5b836afa0b4e71be66b595482a7
Pratyush005 <pandapratyush005@gmail.com>
2025-04-25 22:23:35 +0530

    text changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,40 @@
 export default function Home() {
   return (
-    <div className="banner absolute w-full h-screen flex items-center justify-center">
-      <video autoPlay loop muted className="absolute top-0 left-0 w-full h-full object-cover">
-        <source src="/assets/videos/jetcenter.mp4" type="video/mp4" />
-      </video>
-      <h2 className="absolute inset-0 flex items-center justify-center bg-white bg-opacity-60 text-[240px] uppercase font-[700]">
-        Csm
-      </h2>
-    </div>
+  <>
+     <header>
+      <div className="header-wrap">
+        <video
+          autoPlay
+          muted
+          loop
+         >
+          <source type="video/mp4" src="/assets/videos/jetcenter.mp4" />
+        </video>
+        <svg viewBox="0 0 160 90">
+          <g className="header-backdrop" mask="url(#header-mask)">
+            <rect id="cover" x="-5%" y="-5%" width="110%" height="110%" />
+          </g>
+          <g className="transparent-text">
+            <text
+              id="heading"
+              className="heading-text"
+              text-anchor="middle"
+              x="50%"
+              y="50%"
+              dy="0.3em">
+              CSM
+            </text>
+          </g>
+          <mask id="header-mask">
+            <use href="#cover" style={{fill:"white"}} />
+
+            <use href="#heading" />
+       
+            <use href="#text-path" style={{fill:"white"}} />
+          </mask>
+        </svg>
+      </div>
+    </header>
+  </>
   );
 }
