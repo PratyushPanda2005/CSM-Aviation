f34667914ab567bddedac4923294ffc7ce658d29
Pratyush005 <pandapratyush005@gmail.com>
2025-04-26 12:35:25 +0530

    plane transition modification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,38 @@
 'use client'
 import Image from "next/image";
 import Plane from "../../public/assets/videos/plane.png";
-import Cloud from "../../public/assets/videos/cloud2.png";
 import { motion } from "framer-motion";
 
 export default function Home() {
   return (
-    <div className="overflow-hidden bg-black h-screen flex items-center">
-      <header className="relative w-full h-full">
-        {/* Cloud that stops in the middle */}
+    <div className="overflow-hidden w-screen bg-black h-screen flex items-center">
+      <div className="relative w-full h-full">
+        {/* Yellow trail that follows the plane */}
         <motion.div
           initial={{ x: "-100%" }}
-          animate={{ x: "50%" }}
+          animate={{ x: "0%" }}
           transition={{
-            duration: 2.4,
-            delay: 1.6,
-           
+            duration: 3.1,
+            ease: "linear",
+            delay: 1.8
           }}
-          className="absolute top-1/2 -translate-y-1/2 w-[800px]"
-        >
-          <Image src={Cloud} alt="Cloud" className="w-full h-auto" />
-        </motion.div>
-
-        {/* Plane that continues moving */}
+          className="absolute top-1/2 -translate-y-1/2 h-[300px] w-screen bg-amber-900 flex justify-center items-center text-9xl text-amber-200 font-[700]"
+        >HELLO</motion.div>
+        
+        {/* Plane that moves across */}
         <motion.div
           initial={{ x: "-100%" }}
           animate={{ x: "300%" }}
           transition={{
-            duration: 4,
+            duration: 6,
             ease: "linear",
-            delay: 0.5
+           
           }}
-          className="absolute top-1/2 -translate-y-1/2 "
+          className="absolute top-1/2 -translate-y-1/2 w-auto"
         >
           <Image src={Plane} alt="Plane" className="w-full h-auto" />
         </motion.div>
-      </header>
+      </div>
     </div>
   );
 }
