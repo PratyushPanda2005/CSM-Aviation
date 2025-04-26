3eeb9db99559e83ac164318efe3d8e299e2f2c10
Pratyush005 <pandapratyush005@gmail.com>
2025-04-26 20:52:27 +0530

    Pathway changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import Image from "next/image";
 import Plane from "../../public/assets/videos/plane.png";
+import Pathway from "../../public/assets/videos/Road.jpeg"
 import { motion } from "framer-motion";
 
 export default function Home() {
@@ -8,18 +9,37 @@ export default function Home() {
     <div className="overflow-hidden w-screen bg-white h-screen flex items-center">
       <div className="relative w-full h-full">
         {/* Yellow trail that follows the plane */}
+        <div
+        
+          className="absolute top-1/2 -translate-y-1/2 h-[400px] w-screen bg-[#101218]   text-5xl lg:text-7xl xl:text-9xl  font-[700]"
+        >
+          <div className="w-full h-full flex justify-between flex-col">
+         <Image src={Pathway} className="w-full h-full" alt=""/>
+          </div>
+        
+        </div>
+        
+        {/* Plane that moves across */}
         <motion.div
           initial={{ x: "-100%" }}
-          animate={{ x: "0%" }}
+          animate={{ x: "300%" }}
           transition={{
-            duration: 3.1,
+            duration: 6,
             ease: "linear",
-            delay: 1.8
+           
           }}
-          className="absolute top-1/2 -translate-y-1/2 h-[400px] w-screen bg-[#101218] border-y-[32px] border-[#C7B299] text-5xl lg:text-7xl xl:text-9xl text-amber-200 font-[700]"
+          className="absolute top-1/2 -translate-y-1/2 w-auto"
         >
-          <div className="w-full h-full py-4 flex justify-between flex-col">
-          <div className="flex">
+          <Image src={Plane} alt="Plane" className="w-full h-auto" />
+        </motion.div>
+      </div>
+    </div>
+  );
+}
+
+
+{/*
+   <div className="flex">
           <div className="bg-yellow-300 h-[12px] w-full"/>
           </div>
           <div className="flex justify-evenly">
@@ -46,24 +66,4 @@ export default function Home() {
           <div className="flex">
           <div className="bg-yellow-300 h-[12px] w-full"/>
           </div>
-          </div>
-        
-        </motion.div>
-        
-        {/* Plane that moves across */}
-        <motion.div
-          initial={{ x: "-100%" }}
-          animate={{ x: "300%" }}
-          transition={{
-            duration: 6,
-            ease: "linear",
-           
-          }}
-          className="absolute top-1/2 -translate-y-1/2 w-auto"
-        >
-          <Image src={Plane} alt="Plane" className="w-full h-auto" />
-        </motion.div>
-      </div>
-    </div>
-  );
-}
+  */}
